8a6357342270d959ec1b4e232ff7a43c683a5708
chananup <chananup@sheridancollege.ca>
2021-01-29 11:52:12 -0500

    Added private variable address and getter and setter

--- src/softwaredesignarray/SudentClass.java
+++ src/softwaredesignarray/SudentClass.java
@@ -11,6 +11,15 @@ package softwaredesignarray;
  */
 public class SudentClass {
     private String name;
+    private String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
     
     
     public String getName(){
